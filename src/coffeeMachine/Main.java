8007d447263da4ffe9d6dc4a6984623d4bebdba8
DianaBogdanova98 <diankabogdanova@mail.ru>
2018-10-14 20:29:16 +0300

    commit

--- src/coffeeMachine/Main.java
+++ src/coffeeMachine/Main.java
@@ -1,7 +1,23 @@
 package coffeeMachine;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int cups = scanner.nextInt();
+        int water = cups * 200;
+        int milk = cups * 50;
+        int coffee = cups * 15;
+        System.out.print("Write how many cups of coffee you will need: ");
+        System.out.println(cups);
+        System.out.print ("For ");
+        System.out.print (cups);
+        System.out.println (" cups you will need: ");
+        System.out.print (water);
+        System.out.println (" ml of water");
+        System.out.print (milk);
+        System.out.println (" ml of milk ");
+        System.out.print (coffee);
+        System.out.println(" g of coffee beans");
+
     }
 }
